5f2dbbf38daf2b3ca4314b6d68c3da1cfa6dc9c5
denislgx <gabrieldenislopez@gmail.com>
2020-11-24 18:11:28 -0300

    Seeder Script

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+    {
+        name: "Admin User",
+        email: "adming@example.com",
+        password: bcrypt.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name: "Ernest Hemingway",
+        email: "ehemingway@example.com",
+        password: bcrypt.hashSync("123456", 10),
+    },
+    {
+        name: "Anne Carson",
+        email: "acarson @example.com",
+        password: bcrypt.hashSync("123456", 10),
+    },
+];
+
+export default users;
