f1c9d03a5a0723fe81eb17de99eb7e635cab417c
denislgx <gabrieldenislopez@gmail.com>
2020-11-25 12:07:07 -0300

    Message & Loading Components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 import { Row, Col } from "react-bootstrap";
 
@@ -19,9 +21,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest products:</h1>
             {loading ? (
-                <h2>LOADING</h2>
+                <Loader />
             ) : error ? (
-                <h3>{error}</h3>
+                <Message variant="danger">{error}</Message>
             ) : (
                 <Row>
                     {products.map((product, index) => (
