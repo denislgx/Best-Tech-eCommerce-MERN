9157c831620ec9bd0d2ebe49ca1fdda864e18c49
denislgx <gabrieldenislopez@gmail.com>
2020-12-04 11:21:26 -0300

    Admin User List Screen / Admin User Dropdown (Header) / Action & Reducer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -47,6 +47,25 @@ const Header = () => {
                                     </Nav.Link>
                                 </LinkContainer>
                             )}
+                            {userInfo?.isAdmin && (
+                                <NavDropdown title="Admin" id="adminmenu">
+                                    <LinkContainer to="/userlist">
+                                        <NavDropdown.Item>
+                                            Users
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to="/admin/productlist">
+                                        <NavDropdown.Item>
+                                            Products
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to="/admin/orderlist">
+                                        <NavDropdown.Item>
+                                            Orders
+                                        </NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
