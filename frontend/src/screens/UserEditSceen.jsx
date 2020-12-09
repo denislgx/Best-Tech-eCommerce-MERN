63600030e10679f1fcf969d72a4aaa4887f57e72
denislgx <gabrieldenislopez@gmail.com>
2020-12-09 17:37:51 -0300

    Update User Functionality

--- frontend/src/screens/UserEditSceen.jsx
+++ frontend/src/screens/UserEditSceen.jsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
 
 const UserEditScreen = ({ history, match }) => {
     const [name, setName] = useState("");
@@ -17,19 +18,30 @@ const UserEditScreen = ({ history, match }) => {
     const userId = match.params.id;
 
     const { loading, error, user } = useSelector((state) => state.userDetails);
+    const {
+        loading: loadingUpdate,
+        error: errorUpdate,
+        success: successUpdate,
+    } = useSelector((state) => state.userUpdate);
 
     useEffect(() => {
-        if (!user.name || user._id !== userId) {
-            dispatch(getUserDetails(userId));
+        if (successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET });
+            history.push("/admin/userlist");
         } else {
-            setName(user.name);
-            setEmail(user.email);
-            setIsAdmin(user.isAdmin);
+            if (!user.name || user._id !== userId) {
+                dispatch(getUserDetails(userId));
+            } else {
+                setName(user.name);
+                setEmail(user.email);
+                setIsAdmin(user.isAdmin);
+            }
         }
-    }, [dispatch, user, userId]);
+    }, [dispatch, history, user, userId, successUpdate]);
 
     const submitHandler = (event) => {
         event.preventDefault();
+        dispatch(updateUser({ _id: userId, name, email, isAdmin }));
     };
 
     return (
@@ -39,6 +51,10 @@ const UserEditScreen = ({ history, match }) => {
             </Link>
             <FormContainer>
                 <h1>Edit User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && (
+                    <Message variant="danger">{errorUpdate}</Message>
+                )}
                 {loading ? (
                     <Loader />
                 ) : error ? (
