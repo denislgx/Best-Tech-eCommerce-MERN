4617fc9cf46a98d1219c74fd036b28d5c6a2fcd2
denislgx <gabrieldenislopez@gmail.com>
2020-12-11 11:34:15 -0300

    Admin Order List Screen & Funcionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,3 +85,12 @@ export const getMyOrders = asyncHandler(async (req, res) => {
 
     res.json(orders);
 });
+
+// @desc   Get all orders
+// @route  GET /api/orders
+// @access Private/Admin
+export const getAllOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate("user", "id name");
+
+    res.json(orders);
+});
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -14,6 +14,9 @@ import {
     ORDER_LIST_USER_SUCCESS,
     ORDER_LIST_USER_FAIL,
     ORDER_LIST_USER_RESET,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+    ORDER_LIST_FAIL,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -62,7 +65,7 @@ export const orderPayReducer = (state = {}, action) => {
     }
 };
 
-export const orderListUser = (state = { orders: [] }, action) => {
+export const orderListUserReducer = (state = { orders: [] }, action) => {
     switch (action.type) {
         case ORDER_LIST_USER_REQUEST:
             return { loading: true };
@@ -72,6 +75,19 @@ export const orderListUser = (state = { orders: [] }, action) => {
             return { loading: false, error: action.payload };
         case ORDER_LIST_USER_RESET:
             return { orders: [] };
+        default:
+            return state;
+    }
+};
+
+export const orderListReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+        case ORDER_LIST_REQUEST:
+            return { loading: true };
+        case ORDER_LIST_SUCCESS:
+            return { loading: false, orders: action.payload };
+        case ORDER_LIST_FAIL:
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
